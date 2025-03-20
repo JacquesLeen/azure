c2edd2f20568551fbcc4058ece66c7cba48fc758
JacquesLeen <56220139+JacquesLeen@users.noreply.github.com>
2025-03-20 13:57:39 +0100

    finished 0104

--- Azure_ML_Cloud/01_Create_ML_Models_in_Azure/tools.py
+++ Azure_ML_Cloud/01_Create_ML_Models_in_Azure/tools.py
@@ -49,7 +49,8 @@ def show_density(data, column):
     plt.axvline(x=median, color='mediumblue', linestyle='--', linewidth=2, label='median')
     plt.axvline(x=mode, color='navy', linestyle='--', linewidth=2, label='mode')
     plt.legend()
-    plt.title(f'Densty Plot {column}')
+    plt.xlabel(f'{column.capitalize()}')
+    plt.title(f'Density Plot {column.capitalize()}')
     return ax
 
     
@@ -108,7 +109,143 @@ def show_distribution(data, column):
                 x=data[column],
                 color= 'powderblue',
                 linewidth=2)
-    ax[1].set_xlabel(f'{column}')
+    ax[1].set_xlabel(f'{column.capitalize()}')
 
-    fig.suptitle('Distribution Data')
-    fig.show()
+    fig.suptitle(f'{column.capitalize()} Distribution Data')
+    return fig
+
+
+def show_distribution_cat_feature(df, column):
+    """
+    Plots the distribution of a categorical feature using a barplot and highlights the count of each category.
+
+    Parameters:
+    ----------
+    df : pandas.DataFrame
+        The DataFrame containing the data.
+    column : str
+        The column representing the categorical feature to plot.
+
+    Returns:
+    -------
+    matplotlib.axes._subplots.AxesSubplot
+        The Axes object containing the barplot.
+
+    Example:
+    --------
+    >>> import pandas as pd
+    >>> import seaborn as sns
+    >>> import matplotlib.pyplot as plt
+    >>> data = pd.DataFrame({'City': ['New York', 'New York', 'LA', 'LA', 'New York', 'LA']})
+    >>> ax = show_distribution_cat_feature(data, column='City')
+    >>> plt.show()
+    """
+    data = df[column].value_counts().sort_index().reset_index()
+    palette_length = data.shape[0]
+    diverging_colors = sns.color_palette("Blues", n_colors=palette_length)
+    plt.style.use('fivethirtyeight')
+
+    fig, ax = plt.subplots(figsize=(10, 4))
+    sns.barplot(data, 
+                x=column, 
+                y='count',
+                palette= diverging_colors
+                )
+    ax.set_ylabel('Count')
+    ax.set_xlabel(column.capitalize())
+    ax.set_title(f'{column.capitalize()} Distribution')
+    return fig
+
+def show_correlation_with_target(data, target, column):
+    """
+    Plots the correlation between a specified column and the target column using a scatter plot, and displays the correlation coefficient.
+
+    Parameters:
+    ----------
+    data : pandas.DataFrame
+        The DataFrame containing the data.
+    target : str
+        The target column to correlate with.
+    column : str
+        The column for which to plot the correlation against the target.
+
+    Returns:
+    -------
+    matplotlib.figure.Figure
+        The figure object containing the scatter plot.
+
+    Example:
+    --------
+    >>> import pandas as pd
+    >>> import seaborn as sns
+    >>> import matplotlib.pyplot as plt
+    >>> data = pd.DataFrame({'Target': [1, 2, 3, 4, 5], 'Feature': [2, 3, 4, 5, 6]})
+    >>> fig = show_correlation_with_target(data, target='Target', column='Feature')
+    >>> plt.show()
+    """
+    plt.style.use('fivethirtyeight')
+    corr = data[column].corr(data[target])
+    fig, ax = plt.subplots(figsize=(10, 4))
+    sns.scatterplot(data=data,
+                    x=target,
+                    y=column,
+                    color='royalblue',
+                    ax=ax)
+    ax.set_ylabel(column.capitalize())
+    ax.set_xlabel(target.capitalize())
+    ax.set_title(f'{column.capitalize()} Correlation with {target.capitalize()} ({round(corr, 2)})')
+    return fig
+
+def show_correlation_cat_with_target(data, target, column):
+    """
+    Plots the correlation between a categorical column and the target column using a boxplot.
+
+    Parameters:
+    ----------
+    data : pandas.DataFrame
+        The DataFrame containing the data.
+    target : str
+        The target column to correlate with.
+    column : str
+        The categorical column for which to plot the correlation against the target.
+
+    Returns:
+    -------
+    matplotlib.figure.Figure
+        The figure object containing the boxplot.
+
+    Example:
+    --------
+    >>> import pandas as pd
+    >>> import seaborn as sns
+    >>> import matplotlib.pyplot as plt
+    >>> data = pd.DataFrame({'Target': [1, 2, 3, 4, 5], 'Category': ['A', 'B', 'A', 'B', 'A']})
+    >>> fig = show_correlation_cat_with_target(data, target='Target', column='Category')
+    >>> plt.show()
+    """
+    
+    plt.style.use('fivethirtyeight')
+    # Determine the number of unique categories in the column
+    n_colors = data[column].value_counts().sort_index().reset_index().shape[0]
+    
+    # Generate a color palette based on the number of categories
+    palette = sns.color_palette("Blues", n_colors=n_colors)
+    
+    # Create a figure and axes
+    fig, ax = plt.subplots(figsize=(10, 4))
+    
+    # Create the boxplot on the axes
+    sns.boxplot(data=data,  # Corrected 'bike_data' to 'data'
+                x=column,
+                y=target,
+                palette=palette,
+                linewidth=2,
+                ax=ax)
+    
+    # Set the title and labels
+    ax.set_title(f'Boxplot for Correlation between {column.capitalize()} and {target.capitalize()}')
+    ax.set_xlabel(column.capitalize())
+    ax.set_ylabel(target.capitalize())
+    
+    # Return the figure object
+    return fig
